e3a6c9f97f1c2006fd176bbe47d108e344a24a32
Vu Duc Le <97.vuducle@gmail.com>
2020-08-31 00:17:20 +0200

    calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,8 +16,53 @@ function Calculator() {
         )
     })
     return (
-        <div>
-            
+        <div className="wrapper">
+            <div className="show-input">
+                {data}
+            </div>
+            <div className="digits flex">
+                {calculatorBtns}
+            </div>
+            <div className="modifiers subgrid">
+                <button onClick={() => setData(data.substr(0, data.length - 1))}>
+                    Clear
+                </button>
+                <button onClick={() => setData("")}>
+                    AC
+                </button>
+            </div>
+
+            <div className="operations subgrid">
+                <button onClick={e => setData(data + e.target.value)} value="+">
+                +
+                </button>
+                <button onClick={e => setData(data + e.target.value)} value="-">
+                -
+                </button>
+                <button onClick={e => setData(data + e.target.value)} value="*">
+                *
+                </button>
+                <button onClick={e => setData(data + e.target.value)} value="/">
+                /
+                </button>
+
+                <button onClick={
+                    e => {
+                        try {
+                            setData(
+                                String(eval(data)).length > 3 &&
+                                    String(eval(data)).includes(".")
+                                    ? String(eval(data).toFixed(4))
+                                    : String(eval(data))
+                                    );
+                        } catch(err) {
+                            console.log(err);
+                        }
+                    }
+                } value="=">
+                    =
+                </button>
+            </div>
         </div>
     )
 }
